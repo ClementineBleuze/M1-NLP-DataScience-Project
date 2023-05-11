4bac40ce5635f8ac250436f4757d968438234433
ClementineBleuze <clementinebleuze@gmail.com>
2023-05-11 19:29:36 +0200

    POS tagging (to be finished)

--- code/util.py
+++ code/util.py
@@ -1,3 +1,18 @@
+# NLTK imports
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import WordPunctTokenizer
+nltk.download('punkt')
+from nltk.tag import pos_tag
+nltk.download('stopwords')
+
+# spaCy imports
+import spacy
+from spacy.tokenizer import Tokenizer
+nlp_spacy =spacy.load("en_core_web_sm")
+
+# Other imports
+import pandas as pd
 import re
 
 ## Text cleaning
@@ -24,3 +39,29 @@ def clean_text(text:str)->str:
     text = remove_excess_whitespace(text)
     text = text.lower()
     return text
+
+
+def get_filtered_tokens_set_NLTK(tokens_set):
+  """A function that returns a set of unique tokens from which we have removed NLTK stopwords and punctuation"""
+
+  stop_words =set(stopwords.words('english'))
+  filtered = set()
+
+# Remove the stop words and punctuation from the tokens set
+  for token in tokens_set:
+      if token not in stop_words and not token.ispunct():
+          filtered.add(token)
+          
+  return filtered
+
+def get_filtered_tokens_set_spacy(tokens_set):
+    """A function that returns a set of unique token recognized by Spacy and then remove the stop word and punctuation """
+  
+    filtered = set()
+    stop_word_spacy = nlp_spacy.Defaults.stop_words
+
+    for token in tokens_set:
+        if not token.is_punct and not token.is_stop:
+            filtered.add(token)
+    
+    return filtered
